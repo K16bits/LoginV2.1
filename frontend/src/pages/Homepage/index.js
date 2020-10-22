0404424c617d4ac8ccfa3259924e6956b4897117
Cristiano Lima <52179531+K16bits@users.noreply.github.com>
2020-10-22 14:15:48 -0300

    Teste no console log para ver os dados

--- frontend/src/pages/Homepage/index.js
+++ frontend/src/pages/Homepage/index.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Title from '../../components/Title'
 import Form from '../..//components/Form'
 import Main from '../../components/Main'
@@ -6,13 +6,32 @@ import Input from '../../components/Input'
 import Button from '../../components/Button'
 
 export default function Homepage(){
+
+    const [nome,setnome] = useState('');
+    const [senha,setsenha] = useState('');
+
+
+    async function HandleRegister(e){
+        e.preventDefault();
+        const data = {
+            nome,
+            senha
+        }
+        console.log(data);
+
+    }
+
     return(
         <Main>
-            <Title>Login</Title>
-            <Form>
-                <Input placeholder='User'></Input>
-                <Input placeholder='Senha' type='password'></Input>
-                <Button>Entrar</Button>           
+            <Form onSubmit={HandleRegister}>
+                <Title>LOGIN</Title>
+                <Input name='user' placeholder='Usuario'
+                    onChange= { e => setnome(e.target.value)}
+                ></Input>
+                <Input name='password' placeholder='Senha' type='password'
+                    onChange = { e => setsenha(e.target.value) }
+                ></Input>
+                <Button type='submit' onSubmit={HandleRegister} >Entrar</Button>
             </Form>
         </Main>
     )
