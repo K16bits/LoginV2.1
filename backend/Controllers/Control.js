84599386a04ebc3d9a7eb69092bd8aa02e7b214c
Cristiano Lima <52179531+K16bits@users.noreply.github.com>
2020-10-23 15:35:51 -0300

    Controle de rotas

---
+++ backend/Controllers/Control.js
@@ -0,0 +1,29 @@
+const connection = require('../Database/connection');
+
+module.exports = {
+    async add(request,response){
+        const {email,senha,nome} = request.body;
+
+       try{
+           await connection('users').insert({
+               email,
+               senha,
+               nome
+            })
+        response.json(nome);
+
+       }catch(e){
+           console.log(e);
+       }
+    },
+
+    async list(request,response){
+        try{
+            const data = await connection('users').select('*');
+            console.log(data);
+            response.json(data)
+        }catch(e){
+            console.log(e);
+        }r
+    }
+}
