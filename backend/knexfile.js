9547aed94df1cb4ee70d6f43bfa21b7c8f1795e6
Cristiano Lima <52179531+K16bits@users.noreply.github.com>
2020-10-23 15:38:12 -0300

    knex init

---
+++ backend/knexfile.js
@@ -0,0 +1,46 @@
+module.exports = {
+
+  development: {
+    client: 'sqlite3',
+    connection: {
+      filename: './Database/users.sqlite3'
+    },
+    migrations:{
+      directory: './Database/migrations'
+    },
+    useNullAsDefault: true
+  },
+
+  staging: {
+    client: 'postgresql',
+    connection: {
+      database: 'my_db',
+      user:     'username',
+      password: 'password'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  },
+
+  production: {
+    client: 'postgresql',
+    connection: {
+      database: 'my_db',
+      user:     'username',
+      password: 'password'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  }
+
+};
