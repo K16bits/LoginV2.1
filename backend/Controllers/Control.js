764e7fe5ece4b8f2f7ab70bddfcc2793a8909eeb
Cristiano Lima <52179531+K16bits@users.noreply.github.com>
2020-10-25 18:23:04 -0300

    controle de delete, Autenticação e criptografia de senha

--- backend/Controllers/Control.js
+++ backend/Controllers/Control.js
@@ -23,14 +23,35 @@ module.exports = {
         try{
             const data = await connection('users').select('*');
             console.log(data);
-            response.json(data)
+            response.json(data);
         }catch(e){
             console.log(e);
         }
     },
 
-    async auther(resquest,response){
+    async remove(request,response){
+        const id =  request.params.id;
+        const resposta = await connection('users').where({'id':id}).delete();
         
-    }
+        if(resposta){
+            response.send("Valor deletado")
+            console.log(true)
+        }else{
+            response.send("Valor não encontrado")
+            console.log(false)
+        }
+        
+    },
 
+    async autherticate(request, response){
+        const {email,senha} = request.body;
+        const data = await connection('users').select("nome","senha").where({'email':email}).first()
+        const resposta = await brcryt.compare(senha,data['senha'])
+
+        if(resposta == true){
+            response.send("Logado");
+        }else{
+            response.send("Errou na senha");
+        }
+    }
 }
