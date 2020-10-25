e673fb2c4f617b55b214e191b52dd28256b1487e
Cristiano Lima <52179531+K16bits@users.noreply.github.com>
2020-10-25 15:21:11 -0300

    rota de delete

--- backend/routes.js
+++ backend/routes.js
@@ -1,9 +1,9 @@
-const { request, response } = require('express');
 const express = require('express')
 const router = express.Router();
 const controllers = require('./Controllers/Control');
 
-router.post("/add",controllers.add)
-router.get("/list",controllers.list)
+router.post("/user",controllers.add)
+router.get("/user",controllers.list)
+router.delete("user/:id",controllers.remove)
 
 module.exports = router;
