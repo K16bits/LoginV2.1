999387fa505eb11a188bac2118654aa1ef8a7d9e
Cristiano Lima <52179531+K16bits@users.noreply.github.com>
2020-10-25 18:21:04 -0300

    Adicionando rota de delete e Autenticação

--- backend/routes.js
+++ backend/routes.js
@@ -4,6 +4,7 @@ const controllers = require('./Controllers/Control');
 
 router.post("/user",controllers.add)
 router.get("/user",controllers.list)
-router.delete("user/:id",controllers.remove)
+router.delete("/user/:id",controllers.remove)
+router.get("/user/logar",controllers.autherticate)
 
 module.exports = router;
