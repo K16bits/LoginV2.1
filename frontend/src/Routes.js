72bfb29a7be543418c7487547e45c3aa91c287ee
Cristiano Lima <52179531+K16bits@users.noreply.github.com>
2020-10-27 18:59:32 -0300

    Rotas de registro

--- frontend/src/Routes.js
+++ frontend/src/Routes.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import {Route,BrowserRouter,Switch} from 'react-router-dom'
 import Homepage from './pages/Homepage/'
+import Register from './pages/Register'
 
 export default function Routes(){
     return(
         <BrowserRouter>
             <Switch>
                 <Route path='/' exact component={Homepage} />
+                <Route path='/register' component={Register}/>
             </Switch>
         </BrowserRouter>
     )
---
+++ frontend/src/pages/Register/index.js
@@ -0,0 +1,50 @@
+import React, { useState } from 'react'
+import Form from '../../components/Form'
+import Input from '../../components/Input'
+import Button from '../../components/Button'
+
+import api from '../../services/api'
+import Main from '../../components/Main'
+
+export default function Register(){
+
+    const [email,setemail] = useState('')
+    const [senha,setsenha] = useState('')
+    const [nome,setnome] = useState('')
+
+    async function HandleRegister(e){
+        e.preventDefault()
+
+        const data = {
+            email,
+            senha,
+            nome
+        }
+        console.log(data);
+        
+        try{
+           const resposta =  await api.post("/user",data)
+           alert(`Cadastro realizado Sr. ${resposta.data.nome}`)
+        }catch(e){
+            console.log(e)
+        }
+    }
+
+    return(
+       <Main>
+         <Form onSubmit={HandleRegister}>
+            <Input value={email} placeholder="Email"
+                onChange = { e => setemail(e.target.value) }
+            ></Input>
+            <Input value={senha} type='password' placeholder="senha"
+                onChange= { e => setsenha(e.target.value)}
+            ></Input>
+            <Input value={nome}  placeholder="nome"
+                onChange ={ e => setnome(e.target.value ) }
+            ></Input>
+            <Button onSubmit={HandleRegister}>Enviar</Button>
+         </Form>
+       </Main>
+        
+    )
+}
